fa7d1fe4a74456dcea98e63aae1758b0e34a778d
Charles <c1mckay@sdsc.edu>
2016-12-12 20:21:18 -0800

    removed unused import

--- app/src/main/java/android/alerts/RoleCardPopUp.java
+++ app/src/main/java/android/alerts/RoleCardPopUp.java
@@ -10,7 +10,6 @@ import android.app.DialogFragment;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
